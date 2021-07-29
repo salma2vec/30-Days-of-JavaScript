f55b40e1eaacff8a1d2290e7fa874f7a7ed0ed52
Salma <salmasaa02@gmail.com>
2021-07-29 20:22:42 +0530

    Create drum.js

---
+++ drum.js
@@ -0,0 +1,31 @@
+window.addEventListener('keydown' , playDrum);
+function playDrum(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.add('playing');
+};
+
+// Remove .playing class after transition action over
+const keys = document.querySelectorAll('.key');
+keys.forEach( key => key.addEventListener('transitionend' , removePlayingClass ) );
+function removePlayingClass(e)
+{
+    //if( e.propertyName != 'transform' ) return;
+    this.classList.remove('playing');
+}
+
+
+// Play music by mouse click
+keys.forEach( key => key.addEventListener('click' , playByMouse ) );
+function playByMouse(e)
+{
+    const keyCode = this.getAttribute('data-key');
+	const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+	audio.currentTime = 0;
+	audio.play();
+	const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+    key.classList.add('playing');
+}
